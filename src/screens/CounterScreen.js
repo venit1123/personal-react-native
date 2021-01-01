89d06c6a4ee7bd85c17cdd58f3911e35ec2deb9b
Itzel Ventura <venit1123@gmail.com>
2021-01-01 14:44:56 -0800

    Modified CounterScreen to use useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,54 @@
-import React, { useState } from 'react'
-import {View, Text, StyleSheet, Button, FlatList} from 'react-native'
+import React, { useReducer } from 'react'
+import {View, StyleSheet, FlatList} from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
+const DEFAULT_INCREMENT = 25
+
+const reducer = (state, action ) => {
+    /*
+        state === { red: number, green: number, blue: number}
+        action === {colorToChange: 'red' || 'green' || 'blue , amount: 15 || -15} 
+        Note: by convention the community uses the following key values:
+        action  === { type: 'change_red' || 'change_green' || 'change_blue , payload: 15 || -15}
+    */
+
+    switch (action.type) {
+        case 'change_red':
+            // Note: never do state.red = state.red - 15 ... Never change state directly
+            // instead we rebuild our state object 
+            return state.red + action.payload > 255 || state.red + action.payload < 0 
+            ? state // always return something
+            : { ...state, red: state.red + action.payload }
+
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0
+            ? state
+            : { ...state, green: state.green + action.payload }
+
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
+            ? state 
+            : { ...state, blue: state.blue + action.payload }
+
+        default:
+            // remember to ALWAYS return a new object to use as state
+            return state
+    }
+
+}
+
 const CounterScreen = () => {
+
     const color_list = [ 
-        { color: 'Red'},
-        { color: 'Green' },
-        { color: 'Blue' }
+        { color: 'red'},
+        { color: 'green' },
+        { color: 'blue' }
     ]
 
-    const [red, setRed] = useState(0)
-    const [green, setGreen] = useState(0)
-    const [blue, setBlue] = useState(0)
-
-    const COLOR_INCREMENT = 15
+    //useReducer example:
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0})
+    // destructuring
+    const { red, green, blue } = state
 
     return(
         <View>
@@ -24,41 +59,14 @@ const CounterScreen = () => {
                     return (
                         <ColorCounter 
                             color={item.color}
-
-                            onIncrease={(color) => {
-                                switch (color){
-                                    case 'Red':
-                                        setRed(red + COLOR_INCREMENT)
-                                        break
-                                    case 'Green':
-                                        setGreen(green + COLOR_INCREMENT)
-                                        break
-                                    case 'Blue':
-                                        setBlue(blue + COLOR_INCREMENT)
-                                        break
-                                    }
-                                }
-                            }
-                            onDecrease={(color) => {
-                                switch (color) {
-                                    case 'Red':
-                                        setRed(red - COLOR_INCREMENT)
-                                        break
-                                    case 'Green':
-                                        setGreen(green - COLOR_INCREMENT)
-                                        break
-                                    case 'Blue':
-                                        setBlue(blue - COLOR_INCREMENT)
-                                        break
-                                    }
-                                }
-                            }
+                            onIncrease={() => dispatch({ type: `change_${item.color}`, payload: DEFAULT_INCREMENT}) }
+                            onDecrease={() => dispatch({ type: `change_${item.color}`, payload: -1 * DEFAULT_INCREMENT })}
                         /> 
                     )
                 }}>
             </FlatList>
             <View
-                style={{
+                style = {{
                     height: 150,
                     width: 150,
                     backgroundColor: `rgb(${red}, ${green}, ${blue})`
@@ -69,8 +77,12 @@ const CounterScreen = () => {
     )
 }
 
-// const style = StyleSheet.create({
-
-// })
+const style = StyleSheet.create({
+    styleIncrementInput : {
+        height: 40,
+        borderColor: 'gray', 
+        borderWidth: 1
+    }
+})
 
 export default CounterScreen
